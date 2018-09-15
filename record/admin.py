40d5c4887546d4eed3a0ecae6323cc11ec9ca9c6
chengyumeng <792400644@qq.com>
2018-09-16 02:31:40 +0800

    增加后台管理

--- record/admin.py
+++ record/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import  *
+
+class DBAdmin(admin.ModelAdmin):
+    list_display = []
+admin.site.register(Club,DBAdmin)
+admin.site.register(Record,DBAdmin)
+admin.site.register(Socker,DBAdmin)
+admin.site.register(Goal,DBAdmin)
+admin.site.register(Assist,DBAdmin)
+admin.site.register(Competition,DBAdmin)
 
 # Register your models here.
